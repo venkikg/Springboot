69a1170bc242cdb92cdfefda2c35bb2642542c2d
venkatesh <venkikg@gmail.com>
2018-10-14 20:04:02 +0530

    creating new project for m4 named secureui

---
+++ springboot-springcloud-m4-secureui/src/main/java/com/victory/cloudconfig/SpringbootSpringcloudM4SecureuiApplication.java
@@ -0,0 +1,12 @@
+package com.victory.cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringcloudM4SecureuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringcloudM4SecureuiApplication.class, args);
+	}
+}
