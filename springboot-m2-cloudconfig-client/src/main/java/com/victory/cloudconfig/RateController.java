7bd11efd0e62297cd10720e326de945786db8496
venkatesh <venkikg@gmail.com>
2018-10-14 19:59:25 +0530

    committing code modifications related to the module two

--- springboot-m2-cloudconfig-client/src/main/java/com/victory/cloudconfig/RateController.java
+++ springboot-m2-cloudconfig-client/src/main/java/com/victory/cloudconfig/RateController.java
@@ -1,12 +1,10 @@
 package com.victory.cloudconfig;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RefreshScope
 @Controller
 public class RateController {
 	
